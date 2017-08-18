b5229adf6f507c166f681938e2b421d31017df7f
KrausGretzky <antenglong@gmail.com>
2017-08-18 21:43:01 +0800

    __wrap_malloc for detect memory leak

---
+++ makefile/wrap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int g_MemoryCount = 0;
+
+void *__real_malloc(size_t size);
+
+void *__wrap_malloc(size_t size)
+{
+    g_MemoryCount++;
+    printf("__wrap_malloc called\n");
+    return __real_malloc(size);
+}
+
+void __real_free(void* ptr);
+
+void __wrap_free(void* ptr)
+{
+    g_MemoryCount--;
+    printf("__wrap_free called\n");
+    return __real_free(size);
+}
